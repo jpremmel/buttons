d7314fd52830a076ec151fe7c0eed19d3c039f87
Jacqueline Remmel and Lauren Musante <jacqueline@>
2019-08-13 12:51:38 -0700

    Make previous image disappear upon button click and update CSS styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,32 @@
 $(document).ready(function() {
 
   $("#dogbutton").click(function() {
+    $("img").hide();
     $("#dogimg").show();
   });
 
   $("#slothbutton").click(function() {
+    $("img").hide();
     $("#slothimg").show();
   });
 
   $("#goatbutton").click(function() {
+    $("img").hide();
     $("#goatimg").show();
   });
 
   $("#cowbutton").click(function() {
+    $("img").hide();
     $("#cowimg").show();
   });
 
   $("#lorisbutton").click(function() {
+    $("img").hide();
     $("#lorisimg").show();
   });
 
   $("#abutton").click(function() {
+    $("img").hide();
     $("#aimg").show();
   });
 
