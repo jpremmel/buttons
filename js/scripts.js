adcf83c7e5492865126d8bafff25352a446ff2b2
Jacqueline Remmel and Lauren Musante <lauren@>
2019-08-13 11:50:24 -0700

    Add JS for animal buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,27 @@
 $(document).ready(function() {
 
-  
+  $("#dogbutton").click(function() {
+    $("#dogimg").show();
+  });
 
+  $("#slothbutton").click(function() {
+    $("#slothimg").show();
+  });
+
+  $("#goatbutton").click(function() {
+    $("#goatimg").show();
+  });
+
+  $("#cowbutton").click(function() {
+    $("#cowimg").show();
+  });
+
+  $("#lorisbutton").click(function() {
+    $("#lorisimg").show();
+  });
+
+  $("#abutton").click(function() {
+    $("#aimg").show();
+  });
 
 });
